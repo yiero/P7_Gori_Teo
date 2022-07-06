2e269ba678dc9fb348f15df5b4d3f55ca8dc7b89
yiero <goriteo1@hotmail.fr>
2022-07-06 21:50:58 +0200

    Controllers User terminés

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -2,6 +2,8 @@ const db = require('../models');
 const User = db.user;
 const Op = db.Sequelize.Op;
 
+// UPDATE / DELETE à terminer
+
 exports.get = (req, res) => {
   User.findAll()
     .then(user => res.status(200).json(user))
@@ -27,16 +29,27 @@ exports.getOne = (req, res) => {
       });
 };
 
-exports.update = (req, res, next) => {
-  const sequelize = new Sequelize("groupomania", "root", "password", {
-    dialect: "mysql",
-    host: "localhost"
-  });
-  try {
-    sequelize.authenticate();
-  } catch {
-
-  }
+exports.update = (req, res) => {
+  const id = req.params.id;
+  User.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update User with id=${id}. Maybe User wasn't found or req.body is empty.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id
+      });
+    });
 }; 
 
 exports.create = (req, res) => {
@@ -63,16 +76,25 @@ exports.create = (req, res) => {
     });
 };  
 
-exports.delete = (req, res, next) => {
-  const sequelize = new Sequelize("groupomania", "root", "password", {
-    dialect: "mysql",
-    host: "localhost"
-  });
-  try {
-    sequelize.authenticate();
-    let id = req.params.id;
-    sequelize.query(`DELETE FROM utilisateur WHERE id=${id}`).then(() => res.status(200).json({ message: 'utilisateur supprimé' }))
-  } catch {
-    console.log("Suppression échouée code erreur :", error);
-  }
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  User.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete User with id=${id}. Maybe User was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete User with id=" + id
+      });
+    });
 };  
--- back/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-exports.db = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "password",
-        database: "groupomania"
-});
