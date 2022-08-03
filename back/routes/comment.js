5e495df8646be9c7953399d3183eb0c01b363771
yiero <goriteo1@hotmail.fr>
2022-08-03 15:32:46 +0200

    Création page accueil html/css

--- back/routes/comment.js
+++ back/routes/comment.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const commentCtrl = require('../controllers/comment');
+const auth = require('../middleware/auth')
 
-router.get('/', commentCtrl.get);
-router.get('/:id', commentCtrl.getOne);
-router.post('/', commentCtrl.create);
-router.put('/:id', commentCtrl.update);
-router.delete('/:id', commentCtrl.delete);
+router.get('/', auth, commentCtrl.get);
+router.get('/:id', auth, commentCtrl.getOne);
+router.post('/', auth, commentCtrl.create);
+router.put('/:id', auth, commentCtrl.update);
+router.delete('/:id', auth, commentCtrl.delete);
 
 
 module.exports = router;
